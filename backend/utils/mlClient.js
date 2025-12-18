1703304085f3c2b528a84906da4c03a1fbaa6cf4
Akash chandra sarraf <akash17verma2003@gmail.com>
2025-12-18 16:43:15 +0530

    new update

--- backend/utils/mlClient.js
+++ backend/utils/mlClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getMatchScore = async (resumeText, jobText) => {
-  const { data } = await axios.post("http://127.0.0.1:8000/match", {
+  const { data } = await axios.post(`${process.env.ML_SERVICE_URL}/match`, {
     resume_text: resumeText,
     job_text: jobText,
   });
